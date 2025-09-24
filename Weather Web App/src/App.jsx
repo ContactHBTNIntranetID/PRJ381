77c798262ac04f93e08d350fe9fa11f389f19871
OpHlongwane <olethabohlongwan311@gmail.com>
2025-09-25 01:50:53 +0200

    Added frontend (Weather App Folder) with React/Vite setup, axios, leaflet installed

---
+++ Weather Web App/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PretoriaMap from "./components/Maptest"
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Pretoria Map Test</h1>
+      <PretoriaMap />
+    </div>
+  );
+
+
+}
+
+export default App
